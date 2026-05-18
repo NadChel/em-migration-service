5dad729883755e47b8e79b0d68a90011dbfa633b
Sergey Zolotaryov <nad.chel@mail.ru>
2026-05-18 04:18:15 +0300

    Liquibase migration: tables, types, roles

---
+++ src/main/java/com/example/em_migration_service/EmMigrationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.em_migration_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmMigrationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmMigrationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/em_migration_service/EmMigrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.em_migration_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmMigrationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
